129b9cc6d38f005043be0cf3236ba7386895d18e
Corinna Grobe <corinna.grobe@statistik.ji.zh.ch>
2020-04-22 08:37:38 +0200

    Update 20.04.

--- Velo.R
+++ Velo.R
@@ -21,25 +21,25 @@ source("./function_Velo.R")
 # Replace folder name with current folder
 
 #mes 16
-mes16 <- "./Rohdaten_Velo/1704-19042020_Velo/B290pgj - @1018@ - Rohdaten - Dietikon (ZH1018), Radweg (1018_Dietikon).xlsx"
+mes16 <- "./Rohdaten_Velo/20042020_Velo/B290pgj - @1018@ - Rohdaten - Dietikon (ZH1018), Radweg (1018_Dietikon).xlsx"
 mes16_richtung1 <- function_richtung(mes16, 1)
 mes16_richtung2 <- function_richtung(mes16, 2)
 mes16_df <- function_rbind(mes16_richtung1, mes16_richtung2)
 
 #mes 17
-mes17 <-  "./Rohdaten_Velo/1704-19042020_Velo/B290pgj - @2019@ - Rohdaten - Hausen am Albis (ZH2019), Radweg, (2019_Hausen am Albis).xlsx"
+mes17 <-  "./Rohdaten_Velo/20042020_Velo/B290pgj - @2019@ - Rohdaten - Hausen am Albis (ZH2019), Radweg, (2019_Hausen am Albis).xlsx"
 mes17_richtung1 <- function_richtung(mes17, 1)
 mes17_richtung2 <- function_richtung(mes17, 2)
 mes17_df <- function_rbind(mes17_richtung1, mes17_richtung2)
 
 #mes 18
-mes18 <-  "./Rohdaten_Velo/1704-19042020_Velo/B290pgj - @316@ - Rohdaten - Greifensee (ZH0316), Radweg (316_Greifensee).xlsx"
+mes18 <-  "./Rohdaten_Velo/20042020_Velo/B290pgj - @316@ - Rohdaten - Greifensee (ZH0316), Radweg (316_Greifensee).xlsx"
 mes18_richtung1 <- function_richtung(mes18, 1)
 mes18_richtung2 <- function_richtung(mes18, 2)
 mes18_df <- function_rbind(mes18_richtung1, mes18_richtung2)
 
 #mes 19
-mes19 <-  "./Rohdaten_Velo/1704-19042020_Velo/B290pgj - @716@ - Rohdaten - Regensdorf (ZH0716), Radweg, (716_Regensdorf).xlsx"
+mes19 <-  "./Rohdaten_Velo/20042020_Velo/B290pgj - @716@ - Rohdaten - Regensdorf (ZH0716), Radweg, (716_Regensdorf).xlsx"
 mes19_richtung1 <- function_richtung(mes19, 1)
 mes19_richtung2 <- function_richtung(mes19, 2)
 mes19_df <- function_rbind(mes19_richtung1, mes19_richtung2)
